1d1d410fbed4d28385573683cb58a310613e5d64
Sofia Ingersoll ( Sof-aya Ing-er-saul ) <sofia.ingersoll@Outlook.com>
2024-05-01 10:46:36 -0700

    bash programming

---
+++ week3/compute.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# addition function, we will provide the values 
+# when we call bash compute.sh 6 4
+
+first=$1
+second=$2
+echo "The sum of the $first and $second is $(($first+$second))"
